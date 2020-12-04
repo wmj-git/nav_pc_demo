06031b671a6f428ad6afe2c64cd2335afed36a7a
linda <649877013@qq.com>
2020-12-04 10:00:36 +0800

    封装

---
+++ js/common.js
@@ -0,0 +1,10 @@
+/*页面刷新导航高亮不变*/
+function navHighLight(obj) {
+    obj.each(function () {
+        let _winHref = String(window.location);
+        if ($(this)[0].href == _winHref) {
+            $(this).addClass("active").parent('li').siblings().find('a').removeClass("active");
+            $(this).closest('ul').parent("li").addClass("active").siblings().removeClass("active");
+        }
+    });
+}
