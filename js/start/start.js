6452ee8ea947a9dec67bebddd5b7204ee16bbcd2
linda <649877013@qq.com>
2021-01-14 17:26:59 +0800

    按钮动画

--- js/start/start.js
+++ js/start/start.js
@@ -2,18 +2,30 @@ function internationalFn() {
     window.location.href = "international_index.html"
 }
 
+function figure_bottom_Val() {
+    let _height = $(".start_center").innerHeight();
+    let _obj_bottom = parseInt($(".start_center").css('bottom').split('px')[0]);
+    let _bottom = (_height + _obj_bottom) + 'px';
+    return _bottom;
+}
+
 function mouse_enter_Fn() {
     $(".figure.left").css('opacity', 1);
+    let _bototm = figure_bottom_Val();
+    $(".figure_box").css('bottom', _bototm);
 }
 
-function mouse_leave_Fn(e) {
+function mouse_leave_Fn() {
     $(".figure.left").css('opacity', 0);
 }
+
 function mouse_enter_right_Fn() {
     $(".figure.right").css('opacity', 1);
+    let _bototm = figure_bottom_Val();
+    $(".figure_box").css('bottom', _bototm);
 }
 
-function mouse_leave_right_Fn(e) {
+function mouse_leave_right_Fn() {
     $(".figure.right").css('opacity', 0);
 }
 
