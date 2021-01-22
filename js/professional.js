2e56f3de0e656e09f2e7e16a53b9300a04278ef0
linda <649877013@qq.com>
2021-01-22 16:22:46 +0800

    封装

--- js/professional.js
+++ js/professional.js
@@ -1,31 +1,51 @@
-let mySwiper = new Swiper('.banner_swiper', {
-    pagination: {
-        el: '.banner_pagination',
-    },
-    effect: 'fade',
-    autoplay: true,
-    /* observer:true,//修改swiper自己或子元素时，自动初始化swiper
-     observeParents:false,//修改swiper的父元素时，自动初始化swiper
-     onSlideChangeEnd: function(swiper){
-         swiper.update();
-         mySwiper.startAutoplay();
-         mySwiper.reLoop();
-     }*/
+$(function () {
+    bannerSwiper();
+    marginFn();
+    $("#header").load("public/header/firstNav.html");
+    $("#pcMenu").load("public/header/secondNav_pc.html");
+    $("#mobileMenu").load("public/header/secondNav_mobile.html");
+    $("#footer").load("public/footer/footer.html");
+    (function () {
+        $('loadBg').fadeOut();
+        setTimeout(function () {
+            $('loadIcon').hide();
+            $('#loading').fadeOut();
+        }, 800);
+    })();
+    newsSwiper_init();
 });
 
-//鼠标覆盖停止自动切换
-mySwiper.el.onmouseover = function () {
-    mySwiper.autoplay.stop();
+window.onresize = function () {
+    marginFn();
+    newsSwiper_init();
 }
 
-//鼠标离开开始自动切换
-mySwiper.el.onmouseout = function () {
-    mySwiper.autoplay.start();
+function bannerSwiper() {
+    let mySwiper = new Swiper('.banner_swiper', {
+        pagination: {
+            el: '.banner_pagination',
+        },
+        effect: 'fade',
+        autoplay: true
+    });
+    mySwiper.el.onmouseover = function () {
+        mySwiper.autoplay.stop();
+    }
+
+    mySwiper.el.onmouseout = function () {
+        mySwiper.autoplay.start();
+    }
 }
 
 
+function marginFn() {
+    let _portrait_title_width = $(".portrait_title").innerWidth();
+    $(".portrait_title").css("margin-left", (-_portrait_title_width / 2) + 'px');
+    let portrait_flex_item_width = $(".flex_item").innerWidth();
+    $(".flex_item").css({"height": portrait_flex_item_width, 'border-radius': portrait_flex_item_width / 2});
+}
 
-/*新闻中心-一次轮播三个*/
+
 let news_swiper = new Swiper('.news_swiper', {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -38,27 +58,14 @@ let news_swiper = new Swiper('.news_swiper', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    autoplay: true,
-    /* observer:true,//修改swiper自己或子元素时，自动初始化swiper
-     observeParents:false,//修改swiper的父元素时，自动初始化swiper
-     onSlideChangeEnd: function(swiper){
-         swiper.update();
-         news_swiper.startAutoplay();// 重新开始自动切换；
-         news_swiper.reLoop();
-     }*/
+    autoplay: true
 });
 
-function marginFn() {
-    let _portrait_title_width = $(".portrait_title").innerWidth();
-    $(".portrait_title").css("margin-left", (-_portrait_title_width / 2) + 'px');
-    let portrait_flex_item_width = $(".flex_item").innerWidth();
-    $(".flex_item").css({"height": portrait_flex_item_width, 'border-radius': portrait_flex_item_width / 2});
-}
-
-function newsSwiper_mobile() {
+function newsSwiper_init() {
+    /*swiper动态修改属性*/
     let $win = $(window).width();
     if ($win > 768) {
-        news_swiper.destroy(true,true);
+        news_swiper.destroy(true, true);
         news_swiper = new Swiper('.news_swiper', {
             slidesPerView: 3,
             spaceBetween: 30,
@@ -72,7 +79,7 @@ function newsSwiper_mobile() {
             autoplay: true,
         });
     } else {
-        news_swiper.destroy(true,true);
+        news_swiper.destroy(true, true);
         news_swiper = new Swiper('.news_swiper', {
             slidesPerView: 1,
             spaceBetween: 30,
@@ -86,15 +93,4 @@ function newsSwiper_mobile() {
             autoplay: true,
         });
     }
-}
-
-
-$(function () {
-    newsSwiper_mobile();
-    marginFn();
-});
-
-window.onresize = function () {
-    marginFn();
-    newsSwiper_mobile();
 }
